e96b466b3814aa96cb41bf916be00715189f20ec
Husnia Feyzullayeva <husniafeyzullayeva@Husnias-MacBook-Pro.local>
2024-10-03 09:40:27 -0700

    Deploy Vue.js app with db.json API

--- server.js
+++ server.js
@@ -5,27 +5,21 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/api/data', (req, res) => {
-  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).send('error');
-    }
-    res.send(JSON.parse(data));
-  });
-});
 
-app.get('/api/appliances', (req, res) => {
+app.get('/api/data/appliances', (req, res) => {
     fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
       if (err) {
         return res.status(500).send('error');
       }
       res.send(JSON.parse(data).appliances);
     });
-});
+  });
 
 
+  app.use(express.static(path.join(__dirname, 'dist')));
+
+  
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
