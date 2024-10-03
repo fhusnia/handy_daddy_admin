f4de5061fe692498ded1361c036e4b64df1203f3
Husnia Feyzullayeva <husniafeyzullayeva@Husnias-MacBook-Pro.local>
2024-10-03 09:42:15 -0700

    Deploy Vue app with db.json API

--- src/api/appliances.js
+++ src/api/appliances.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://powerful-plains-07667-03abf6521974.herokuapp.com/api/appliances'
+const API_URL = 'https://powerful-plains-07667-03abf6521974.herokuapp.com/api/data/appliances'
 
 export const getAllAppliances = () => {
   return axios.get(API_URL)
