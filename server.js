5610f0fbe5b506ee05ec36259dc3759c5825aae4
Husnia Feyzullayeva <husniafeyzullayeva@Husnias-MacBook-Pro.local>
2024-10-03 04:05:49 -0700

    Added db.json and updated server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/api/data', (req, res) => {
+  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading the database');
+    }
+    res.send(JSON.parse(data));
+  });
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
