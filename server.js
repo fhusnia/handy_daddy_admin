0f8c397139b59c04553a21d54398596e9e972df1
Husnia Feyzullayeva <husniafeyzullayeva@Husnias-MacBook-Pro.local>
2024-10-03 09:48:10 -0700

    Add API to serve appliances data from db.json

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 const express = require('express');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
+// db.json dosyasından verileri okuma endpoint'i
 app.get('/api/data/appliances', (req, res) => {
     fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
       if (err) {
-        return res.status(500).send('error');
+        return res.status(500).send('Error reading db.json');
       }
       res.send(JSON.parse(data).appliances);
     });
-  });
-
-
-  app.use(express.static(path.join(__dirname, 'dist')));
-
-  
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+// Sunucu başlatma
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
