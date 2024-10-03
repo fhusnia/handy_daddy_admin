9e7f3773ba793a8946ee29f9e8f5d427bea2f694
Husnia Feyzullayeva <husniafeyzullayeva@Husnias-MacBook-Pro.local>
2024-10-03 02:34:46 -0700

    Fix: added json-server dependency

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const express = require('express');
-const path = require('path');
 const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // Change to your actual data file
+const middlewares = jsonServer.defaults();
 
-const app = express();
-const router = jsonServer.router('db.json'); // JSON Server için db.json dosyası
-
-app.use(express.static(path.join(__dirname, 'dist'))); // Vue build edilen dosyaları sunma
-app.use('/api', router); // JSON Server'ı /api altında yönlendirme
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+    console.log('JSON Server is running');
 });
